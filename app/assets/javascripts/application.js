33f46a75a5c61b316bde51c69a9a52baca97749a
four-corner <a2.solutions.44@gmail.com>
2015-06-24 08:50:40 +0530

    navbar collapse on small devices

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,11 @@
 //= require turbolinks
 //= require_tree .
 
+$(function() {
+    $('.nav a').on('click', function(){ 
+        if($('.navbar-toggle').css('display') !='none'){
+            $(".navbar-toggle").trigger( "click" );
+        }
+    });
+});
+
